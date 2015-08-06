f8ad931becdbdd1efec5cabf5325c792ea644d39
Ken Snyder <ken@ken.net>
2015-08-06 19:58:46 +0100

    upgrade to 1.13.7 complete

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -13,5 +13,8 @@ module.exports = function(defaults) {
     behave. You most likely want to be modifying `./index.js` or app's build file
   */
 
+  app.import('bower_components/bootstrap/dist/css/bootstrap.css');
+  app.import('bower_components/bootstrap/js/tooltip.js');
+
   return app.toTree();
 };
